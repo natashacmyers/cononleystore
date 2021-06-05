84f6c9ac3d1a0cd6ce5c5d7d5338ce6f6ccacfa4
natashacmyers <natashacmyers@gmail.com>
2021-06-05 11:15:40 +0000

    Customised admin

---
+++ products/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+        'image_url',
+        'sku',
+    )
+
+    ordering = ('name',) # to do a reverse ordering just stick a minus infront of the word 'name'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+
