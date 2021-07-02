dcac002c27b919c56d8d9e60581d39cc51cc1521
natashacmyers <natashacmyers@gmail.com>
2021-07-02 20:11:20 +0000

    Added allowed hosts, Procfile and connected to heroku in CLI

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cononley_store.wsgi:application
--- cononley_store/settings.py
+++ cononley_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w-b*&neuxa(q4ypbfe)gv%)d!tovzljs=%gv2n)v4euzga45vj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://cononley-store.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,18 @@ WSGI_APPLICATION = 'cononley_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 
